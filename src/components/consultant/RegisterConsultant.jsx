d80e04295b0cfd8dbe7144e2946e4a76aaa39240
Jennifer Lima <thalitabrasil872@gmail.com>
2025-06-10 21:23:53 -0400

    static frontend of consultant registration screen completed

--- src/components/consultant/RegisterConsultant.jsx
+++ src/components/consultant/RegisterConsultant.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import "../../css/consultant/register.css";
 
 const RegisterConsultant = () => {
-    const { control, handleSubmit, watch } = useForm();
+    const { control, handleSubmit } = useForm();
     const [step, setStep] = useState(1);
+    const [fileName, setFileName] = useState("");
 
     const onSubmit = (data) => {
         console.log("Form Data:", data);
@@ -13,6 +14,13 @@ const RegisterConsultant = () => {
 
     const nextStep = () => setStep((prev) => Math.min(prev + 1, 3));
     const previousStep = () => setStep((prev) => Math.max(prev - 1, 1));
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setFileName(file.name);
+        }
+    };
 
     return (
         <div className="container-register">
@@ -40,112 +48,157 @@ const RegisterConsultant = () => {
                                     <div className="content-step1-register-consultant">
                                         <Controller
                                             name="name"
+                                            id="name-register-consultant"
                                             control={control}
                                             rules={{ required: "Name is required" }}
                                             render={({ field }) => (
-                                                <input {...field} className="input-name-register-consultant" placeholder="Name"/>
+                                                <input {...field} maxLength='60' className="input-name-register-consultant" placeholder="Name"/>
                                             )}
                                         />
                                         <Controller
                                             name="cpf"
+                                            id="cpf-register-consultant"
                                             control={control}
                                             rules={{ required: "CPF is required" }}
                                             render={({ field }) => (
-                                                <input {...field} className="input-cpf-register-consultant" placeholder="CPF"/>
+                                                <input {...field} maxLength='11' className="input-cpf-register-consultant" placeholder="CPF"/>
                                             )}
                                         />
                                     </div>
                                     <div className="content-step1-register-consultant">
                                         <Controller
                                             name="phone"
+                                            id="phone-register-consultant"
                                             control={control}
                                             rules={{ required: "Phone is required" }}
                                             render={({ field }) => (
-                                                <input {...field} className="input-phone-register-consultant" placeholder="Phone"/>
+                                                <input {...field} maxLength='15' className="input-phone-register-consultant" placeholder="Phone"/>
                                             )}
                                         />
                                         <Controller
                                             name="email"
+                                            id="email-register-consultant"
                                             control={control}
                                             rules={{ required: "Email is required" }}
                                             render={({ field }) => (
-                                                <input {...field} className="input-email-register-consultant" placeholder="Email"/>
+                                                <input {...field} maxLength='60' className="input-email-register-consultant" placeholder="Email"/>
                                             )}
                                         />
                                     </div>
                                 </div>
                                 <div className="subcontainer-step1-register-consultant-down">
-                                    <Controller
-                                    name="image_consultant"
-                                    control={control}
-                                    rules={{ required: "Image URL is required" }}
-                                    render={({ field }) => (
-                                        <input {...field} className="input-image-register-consultant" placeholder="Image URL" />
-                                    )}
+                                   <Controller
+                                        name="image_consultant"
+                                        control={control}
+                                        render={({ field }) => (
+                                            <>
+                                                <input
+                                                    {...field}
+                                                    id="image-consultant-register"
+                                                    className="input-image-register-consultant"
+                                                    type="file"
+                                                    onChange={(e) => {
+                                                        field.onChange(e);
+                                                        handleFileChange(e);
+                                                    }}
+                                                    style={{ display: "none" }}
+                                                />
+                                                <label
+                                                    htmlFor="image-consultant-register"
+                                                    className="custom-file-upload"
+                                                >
+                                                    Escolha uma imagem
+                                                </label>
+                                                {fileName && (
+                                                    <p className="file-name-display">
+                                                        Arquivo selecionado: {fileName}
+                                                    </p>
+                                                )}
+                                            </>
+                                        )}
                                     />
                                 </div>
+
                                 
                             </div>
                         )}
                         {step === 2 && (
                             <div className="step2-register-consultant">
-                                
-                                <Controller
-                                    name="consultants_story"
-                                    control={control}
-                                    rules={{ required: "Consultant's story is required" }}
-                                    render={({ field }) => (
-                                        <textarea {...field} className="input-story-register-consultant" placeholder="Consultant's story"
-                                        />
-                                    )}
-                                />
-                                <Controller
-                                    name="about_specialties"
-                                    control={control}
-                                    rules={{ required: "Specialties are required" }}
-                                    render={({ field }) => (
-                                        <textarea {...field} className="input-specialties-register-consultant" placeholder="About specialties"/>
-                                    )}
-                                />
-                                <Controller
-                                    name="profile_data"
-                                    control={control}
-                                    rules={{ required: "Profile data is required" }}
-                                    render={({ field }) => (
-                                        <textarea {...field} className="input-profile-register-consultant" placeholder="Profile data"/>
-                                    )}
-                                />
-                                
+                                <div className="container-step2-register-consultant-up">
+                                    <Controller
+                                        name="profile_data"
+                                        control={control}
+                                        rules={{ required: "Profile data is required" }}
+                                        render={({ field }) => (
+                                            <textarea {...field} maxLength='800' id="profile-data-register" className="input-profile-register-consultant" placeholder="Fale sobre você"/>
+                                        )}
+                                    />
+                                    <Controller
+                                        name="about_specialties"
+                                        control={control}
+                                        rules={{ required: "Specialties are required" }}
+                                        render={({ field }) => (
+                                            <textarea {...field} maxLength='700' id="about-specialties-register" className="input-specialties-register-consultant" placeholder="Fale sobre suas especialidades"/>
+                                        )}
+                                    /> 
+                                </div>
+                                <div className="container-step2-register-consultant-down">  
+                                    <Controller
+                                        name="consultants_story"
+                                        id="consultants-story-register"
+                                        control={control}
+                                        rules={{ required: "Consultant's story is required" }}
+                                        render={({ field }) => (
+                                            <textarea {...field} maxLength='700' className="input-story-register-consultant" placeholder="Fale sobre sua história como consultor"
+                                            />
+                                        )}
+                                    />
+                                </div>
                             </div>
                         )}
                         {step === 3 && (
                             <div className="step3-register-consultant">
                                 <Controller
                                     name="consultations_carried_out"
+                                    id="consultations-carried-out-register"
                                     control={control}
                                     rules={{ required: "Consultations carried out is required" }}
                                     render={({ field }) => (
-                                        <input {...field} className="input-consultations-register-consultant" placeholder="Consultations carried out"/>
-                                    )}
-                                />
-                                <Controller
-                                    name="payment_plan"
-                                    control={control}
-                                    rules={{ required: "Payment plan is required" }}
-                                    render={({ field }) => (
-                                        <input
-                                            {...field}
-                                            className="input-payment-register-consultant"
-                                            placeholder="Payment Plan"
+                                        <input {...field} 
+                                        type="number"
+                                        maxLength={'4'}
+                                        className="input-consultations-register-consultant" 
+                                        placeholder="Quantas consultas você já realizou?"
                                         />
                                     )}
                                 />
                                 <Controller
+                                name="payment_plan"
+                                id="payment-plan-register"
+                                control={control}
+                                rules={{ required: "Payment plan is required" }}
+                                render={({ field }) => (
+                                    <select
+                                        {...field}
+                                        id="payment-plan-register"
+                                        className="input-payment-register-consultant"
+                                    >
+                                        <option value="" disabled>
+                                            Selecione um plano de pagamento
+                                        </option>
+                                        <option value="plan1">mensal</option>
+                                        <option value="plan2">semestral</option>
+                                        <option value="plan3">anual</option>
+                                    </select>
+                                )}
+                                />
+                                <Controller
                                     name="password"
+                                    id="password-register-consultant"
                                     control={control}
                                     rules={{ required: "Password is required" }}
                                     render={({ field }) => (
-                                        <input {...field} className="input-password-register-consultant" placeholder="Password" type="password"/>
+                                        <input {...field} maxLength='15' className="input-password-register-consultant" placeholder="Password" type="password"/>
                                     )}
                                 />
                             </div>
