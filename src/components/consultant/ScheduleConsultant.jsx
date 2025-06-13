6cc41e3ab668a207c4ce1b5c32d418c789b92576
Jennifer Lima <thalitabrasil872@gmail.com>
2025-06-12 23:29:13 -0400

    Adds schedule fetching functionality in ScheduleConsultant
    Implements a loading state and fetchSchedule function to retrieve consultant schedules from the API using the provided token.
    
    Enhances error handling and updates the state with fetched data upon successful retrieval, improving the user experience by displaying available time slots for consultations.

--- src/components/consultant/ScheduleConsultant.jsx
+++ src/components/consultant/ScheduleConsultant.jsx
@@ -6,6 +6,37 @@ import '../../css/consultant/scheduleConsultant.css'
 
 const ScheduleConsultant = () => {
     const [ schedules, setSchedule ] = useState([])
+    const [ loading, setLoading ] = useState(true)
+    const { token } = useContext(AuthContext);
+    // buscar especialidades do consultor: exibir no array.
+    // buscar agenda com id do consultor especialidade
+    // exibir as datas e horários
+    useEffect(() => {
+        const fetchSchedule = async () => {
+          try {
+            const response = await fetch(`${API}{idConsultantSpecialty}/timeslots`, {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            });
+            if (!response.ok) {
+              throw new Error("Não foi possível buscar suas consultas.");
+            }
+            const data = await response.json();
+            console.log("Consultas:", data.data);
+            setSchedule(data.data);
+          } catch (error) {
+            console.error("Erro ao buscar consultas:", error);
+            
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        // fetchSchedule();
+      }, [token])
     return(
         <div className="container-scheduleConsultant-consultant">
 
